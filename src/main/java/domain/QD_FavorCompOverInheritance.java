f0ea8e927f4def9f489a5619b6d3d63b1e47f298
duvallar <89929057+duvallar@users.noreply.github.com>
2023-10-27 22:06:38 -0400

    Did basic flag of when inheritance is used.

---
+++ src/main/java/domain/QD_FavorCompOverInheritance.java
@@ -0,0 +1,29 @@
+package domain;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+
+import java.io.IOException;
+
+public class QD_FavorCompOverInheritance {
+
+    public static void main(String[] args) throws IOException {
+        String className = "domain/Cat";
+        ClassReader reader = new ClassReader(className);
+        ClassNode classNode = new ClassNode();
+        reader.accept(classNode, ClassReader.EXPAND_FRAMES);
+        checkForInheritance(classNode);
+
+    }
+
+    public static void checkForInheritance(ClassNode classNode){
+        System.out.println("Class: " + classNode.name);
+        String superName = classNode.superName;
+        if (!superName.equals("java/lang/Object")){
+            System.out.println("Consider composition instead of inheritance. This extends " + superName );
+        }
+        System.out.println();
+    }
+
+
+}
