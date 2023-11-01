606c00960a07158e68d3f1ea1606d7785e4980da
Kiana Fan <fank1@rose-hulman.edu>
2023-10-31 21:03:35 -0400

    detects when local variables can be final

--- src/main/java/domain/QDFinalLocalVariables.java
+++ src/main/java/domain/QDFinalLocalVariables.java
@@ -29,7 +29,6 @@ public class QDFinalLocalVariables {
 
     private void checkForFinalLocalVariables(ClassNode classNode) {
         for (MethodNode methodNode : classNode.methods) {
-            System.out.printf("Method: %s\n", methodNode.name);
             checkMethodForFinalLocalVariables(methodNode);
         }
     }
@@ -40,18 +39,27 @@ public class QDFinalLocalVariables {
             localVariables.add(new LocalVariableInfo(localVariableNode.name, localVariableNode.start.getLabel(), localVariableNode.end.getLabel(), localVariableNode.index));
         }
 
+        boolean prevInsnIsLabel = false;
+        AbstractInsnNode prevInsn = null;
         for (AbstractInsnNode insn : methodNode.instructions) {
             if (insn.getNext() != null && insn.getNext().getType() == AbstractInsnNode.LABEL) {
-                System.out.println("setting in scope");
-                // TODO: one error with saying a can be final in testMethod1()
-                // Note for not QD: seems like the variable is getting set as in scope after they are initialized, which is also after the value is set, probably aren't technically in scope while they are being initialized
                 setInScopeVariables(insn.getNext(), localVariables);
             }
+            if (prevInsnIsLabel) {
+                setOutOfScopeVariables(prevInsn, localVariables);
+                prevInsnIsLabel = false;
+            }
+
+            if (insn.getType() == AbstractInsnNode.LABEL) {
+                prevInsnIsLabel = true;
+                prevInsn = insn;
+            }
+
             for (LocalVariableInfo localVariableInfo : localVariables) {
                 if (localVariableInfo.isInScope && storeOpcodes.contains(insn.getOpcode())) {
-                    System.out.printf("name: %s, index: %d, var: %d\n", localVariableInfo.name, localVariableInfo.index, ((VarInsnNode) insn).var);
                     if (localVariableInfo.index == ((VarInsnNode) insn).var) {
                         handleValueStored(localVariableInfo);
+                        break;
                     }
                 }
             }
@@ -68,9 +76,15 @@ public class QDFinalLocalVariables {
         Label instructionLabel = ((LabelNode) insn).getLabel();
         for (LocalVariableInfo localVariableInfo : localVariables) {
             if (localVariableInfo.firstLabel.equals(instructionLabel)) {
-                System.out.printf("%s is in scope\n", localVariableInfo.name);
                 localVariableInfo.isInScope = true;
-            } else if (localVariableInfo.lastLabel.equals(instructionLabel)) {
+            }
+        }
+    }
+
+    private void setOutOfScopeVariables(AbstractInsnNode insn, List<LocalVariableInfo> localVariables) {
+        Label instructionLabel = ((LabelNode) insn).getLabel();
+        for (LocalVariableInfo localVariableInfo : localVariables) {
+            if (localVariableInfo.lastLabel.equals(instructionLabel)) {
                 localVariableInfo.isInScope = false;
             }
         }
@@ -86,10 +100,10 @@ public class QDFinalLocalVariables {
     }
 
     private class LocalVariableInfo {
-        private String name;
-        private Label firstLabel;
-        private Label lastLabel;
-        private int index;
+        private final String name;
+        private final Label firstLabel;
+        private final Label lastLabel;
+        private final int index;
         private boolean isInScope;
         private boolean hasBeenStored;
         private boolean hasBeenStoredOnce;
