bec1767cf78b7aff4a422616679be5f72c4dd8ce
duvallar <89929057+duvallar@users.noreply.github.com>
2023-11-06 21:22:23 -0500

    added default Node

---
+++ src/main/java/domain/MyDefaultInsnNode.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.objectweb.asm.tree.AbstractInsnNode;
+
+public class MyDefaultInsnNode extends MyAbstractInsnNode {
+    // Constructor
+    public MyDefaultInsnNode(AbstractInsnNode node) {
+    }
+
+    @Override
+    public MyAbstractInsnNode getNext() {
+        return null;
+    }
+
+    @Override
+    public int getOpcode() {
+        return 0;
+    }
+
+    @Override
+    public int getType() {
+        return 0;
+    }
+
+    // Any other methods relevant to your default node
+}
