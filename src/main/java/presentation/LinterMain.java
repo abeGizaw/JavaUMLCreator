4c22c3ee9f1ac5acfe59677e4b7acfde9c2bf654
duvallar <89929057+duvallar@users.noreply.github.com>
2023-11-09 08:55:19 -0500

    made template of methods in Main

---
+++ src/main/java/presentation/LinterMain.java
@@ -0,0 +1,44 @@
+package presentation;
+
+import domain.CheckType;
+import domain.Linter;
+import domain.Message;
+
+import java.util.List;
+
+public class LinterMain {
+
+    private Saver saver;
+    private Linter linter;
+    public static void main(String[] args) {
+
+    }
+    private void lint(){}
+
+
+    private String promptUserForDirectoy(){
+        return null;
+    }
+
+    private String promptUserForOutputFileName(){
+        return null;
+    }
+
+    private String promptUserForChecks(){
+        return null;
+    }
+
+    private List<CheckType> validateUserSelectedChecks(String checks){
+        return null;
+    }
+
+    private void prettyPrint(List<Message> messages){
+
+    }
+
+    private void saveToFile(String message){}
+
+
+
+
+}
