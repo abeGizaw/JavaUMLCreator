fe6554e2585af119fe6baa642647892436e25256
Kiana Fan <fank1@rose-hulman.edu>
2023-11-09 08:53:19 -0500

    created Saver and MessageSaver classes

---
+++ src/main/java/datasource/MessageSaver.java
@@ -0,0 +1,12 @@
+package datasource;
+
+public class MessageSaver implements Saver {
+    public MessageSaver(String path) {
+
+    }
+
+    @Override
+    public void saveMessage(String message) {
+
+    }
+}
---
+++ src/test/java/domain/checks/AdapterPatternMockTestClasses/ValidInterfaceTarget/Client.java
@@ -0,0 +1,5 @@
+package domain.checks.AdapterPatternMockTestClasses.ValidInterfaceTarget;
+
+public class Client {
+    private Target target;
+}
