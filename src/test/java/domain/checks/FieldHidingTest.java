9754f935a049db33b8b571f648d1f4f0b13cbd31
rhit-gizaw1 <89957355+rhit-gizaw1@users.noreply.github.com>
2023-11-09 23:31:37 -0500

    testcase for hiddenFields check on class with no hidden fields

--- src/test/java/domain/checks/FieldHidingTest.java
+++ src/test/java/domain/checks/FieldHidingTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class FieldHidingTest {
     MyClassNodeCreator classNodeCreator = new MyASMClassNodeCreator();
     @Test
-    public void hidesFieldsOfVaryingTypes(){
+    public void validateHiddenFieldsCheck_withHiddenFieldsOfDiffTypes_Expect4Hidden(){
         Path basePath= Path.of("G:\\My Drive\\classes\\374SoftwareDesign\\Project\\project-202410-team02-202410\\target\\test-classes\\domain\\checks\\FieldHidingMockClasses\\HiddenFieldsVaryTypes.class");
         validate(basePath, new ArrayList<>(Arrays.asList(
                 "Field similarName is hidden by method doNothing",
@@ -27,7 +27,7 @@ public class FieldHidingTest {
     }
 
     @Test
-    public void hidesFieldsOfSameTypes(){
+    public void validateHiddenFieldsCheck_withHiddenFieldsOfSameTypes_Expect4Hidden(){
         Path basePath= Path.of("G:\\My Drive\\classes\\374SoftwareDesign\\Project\\project-202410-team02-202410\\target\\test-classes\\domain\\checks\\FieldHidingMockClasses\\HiddenFieldsSameTypes.class");
         validate(basePath, new ArrayList<>(Arrays.asList(
                 "Field similarName is hidden by method doNothing",
@@ -36,11 +36,19 @@ public class FieldHidingTest {
                 "Field slightlySimilar is hidden by method doMoreNothing")));
     }
 
+    @Test
+    public void validateHiddenFieldsCheck_withNoHiddenFields_ExpectNoMessages(){
+        Path basePath= Path.of("G:\\My Drive\\classes\\374SoftwareDesign\\Project\\project-202410-team02-202410\\target\\test-classes\\domain\\checks\\FieldHidingMockClasses\\NoHiddenFields.class");
+        MyClassNode classNode = classNodeCreator.createMyClassNodeFromFile(basePath.toFile());
+        FieldHiding fieldHidingCheck = new FieldHiding();
+        List<Message> hiddenFields = fieldHidingCheck.run(classNode);
+        assertEquals(0, hiddenFields.size());
+    }
+
     private void validate(Path basePath, List<String> expectedMessages){
         MyClassNode classNode = classNodeCreator.createMyClassNodeFromFile(basePath.toFile());
         FieldHiding fieldHidingCheck = new FieldHiding();
         List<Message> hiddenFields = fieldHidingCheck.run(classNode);
-        printMessages(hiddenFields);
         for(int i = 0; i < hiddenFields.size(); i ++){
             Message hiddenField = hiddenFields.get(i);
             assertEquals(hiddenField.getCheckType(), CheckType.HIDDEN_FIELDS);
