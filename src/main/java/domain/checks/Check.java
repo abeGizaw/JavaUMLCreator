61329233ab29e797c546b569b35bbfc82b2afda6
rhit-gizaw1 <89957355+rhit-gizaw1@users.noreply.github.com>
2023-11-10 00:33:43 -0500

    small fix

--- src/main/java/domain/checks/Check.java
+++ src/main/java/domain/checks/Check.java
@@ -4,6 +4,6 @@ import domain.Message;
 import domain.MyClassNode;
 import java.util.List;
 public interface Check {
-    public List<Message> run(MyClassNode classNode);
+    public abstract List<Message> run(MyClassNode classNode);
 
 }
