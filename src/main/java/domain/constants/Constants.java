a9d2cb13087ff69f94ddcd40ac449dad292e4aa9
rhit-gizaw1 <89957355+rhit-gizaw1@users.noreply.github.com>
2023-11-14 22:28:24 -0500

    changing the prompt to be more direct

--- src/main/java/domain/constants/Constants.java
+++ src/main/java/domain/constants/Constants.java
@@ -3,7 +3,7 @@ package domain.constants;
 public class Constants {
     public static final String PUML_TYPE = ".puml";
     public static final String OUTPUT_DIRECTORY_FOR_CHECKS = "Please enter an output directory for Checks";
-    public static final String OUTPUT_FOR_PUML_CLASSDIAGRAM = "Please enter an output file path for PUML class diagram";
+    public static final String OUTPUT_FOR_PUML_CLASSDIAGRAM = "Please enter a name for the PUML class diagram";
     public static final String ABBREVIATION_ERROR = "Invalid Input. Please Enter Abbreviations. ";
     public static final String INVALID_PACKAGE = "Invalid package";
     public static final String YES_OR_NO_INPUT_ERROR = "Please say yes(y) or no(n)";
